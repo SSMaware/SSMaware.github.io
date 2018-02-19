a3ac6655243655f7b6cd4b7382b789102bbb20e9
Sandra S. Marler <SandraSMarler@MacBook-Pro-3.frontierlocal.net>
2018-02-18 21:26:27 -0600

    L6Updates
    L6Updates

--- assignments/franklin-6/scripts/currentdate.js
+++ assignments/franklin-6/scripts/currentdate.js
@@ -1,9 +1,8 @@
-var d = new Date();
-var weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
+<html>
+<body>
+<script>
+	 document.getElementById("currentdate").innerHTML = Date();
+	 id = "currentdate";
+</script>
+</body>
+</html>
