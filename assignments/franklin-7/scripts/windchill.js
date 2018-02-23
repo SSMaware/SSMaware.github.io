8ab0eb41216d8df136dc13d4b5e19b54eddfa1bd
Sandra S. Marler <SandraSMarler@MacBook-Pro-3.frontierlocal.net>
2018-02-22 18:24:35 -0600

    L7
    L7

--- assignments/franklin-7/scripts/windchill.js
+++ assignments/franklin-7/scripts/windchill.js
@@ -1,4 +1,7 @@
-/*f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16*/
+/*f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16
+where f is the wind chill factor in Fahrenheit, t is the air average temperature in Fahrenheit, and s is the wind speed in miles per hour.*/
+
+
 function computeNum() {
 	var textInput =
 		get("aNum").value;
@@ -9,3 +12,7 @@ function computeNum() {
 	get ("output").innerHTML = "The value is <strong>" + theValue.toFixed(2) + "</strong>!";
 }
 function get(e) {return document.getElementById(e);}
+
+var t=((h+l)/2)
+f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16
+
