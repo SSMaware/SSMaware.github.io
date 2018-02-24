541d6fb8f85de1a1317d56dacf0607b56c1e58b4
Sandra S. Marler <SandraSMarler@MacBook-Pro-3.frontierlocal.net>
2018-02-24 15:41:26 -0600

    L7 windchill
    L7 windchill

--- assignments/franklin-7/scripts/windchill.js
+++ assignments/franklin-7/scripts/windchill.js
@@ -2,18 +2,18 @@
 where f is the wind chill factor in Fahrenheit, t is the air average temperature in Fahrenheit, and s is the wind speed in miles per hour.*/
 /*double checking*/
 
+var h = document.getElementById("h").innerHTML;
+var l = document.getElementById("l").innerHTML;
+var s = document.getElementById("s").innerHTML;
+h = parseInt(h);
+l = parseInt(l);
+s = parseInt(s);
 
-function computeNum() {
-	var textInput =
-		get("aNum").value;
-	if (textInput.length<3 ||
-	   isNan(textInput)) return;//
-	var theValue = theNumber *
-		Math.pow(Math.random(), 3);
-	get ("output").innerHTML = "The value is <strong>" + theValue.toFixed(2) + "</strong>!";
-}
-function get(e) {return document.getElementById(e);}
+var t = (h + l) / 2;
+var w = Math.pow(s, 0.16);
 
-var t=((h+l)/2)
-f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16
+var f = 35.74 + 0.6215 * t - 35.75 * w + 0.4275 * t * w;
 
+var result = f.toFixed(2);
+
+document.getElementById("windchill").innerHTML = result;
