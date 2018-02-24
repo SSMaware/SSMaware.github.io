b7e4ad88a1c393c0aeead6e6dd335b4ea4fd33b7
Sandra S. Marler <SandraSMarler@MacBook-Pro-3.frontierlocal.net>
2018-02-24 12:12:31 -0600

    L7 currentdate.js
    L7 currentdate.js

--- assignments/franklin-7/scripts/currentdate.js
+++ assignments/franklin-7/scripts/currentdate.js
@@ -1,4 +1,4 @@
-
+/*Double Checking*/
 
 var now = new Date();
 var month = ['January','February','March','April','May','June','July','August','September','October','November','December'];
--- assignments/franklin-7/scripts/windchill.js
+++ assignments/franklin-7/scripts/windchill.js
@@ -1,5 +1,6 @@
 /*f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16
 where f is the wind chill factor in Fahrenheit, t is the air average temperature in Fahrenheit, and s is the wind speed in miles per hour.*/
+/*double checking*/
 
 
 function computeNum() {
