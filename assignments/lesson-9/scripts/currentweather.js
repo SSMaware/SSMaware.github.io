7dcb97002435fe5a7e7a11d23ba0e73c2208b163
Sandra S. Marler <SandraSMarler@MacBook-Pro-3.frontierlocal.net>
2018-03-10 14:47:32 -0600

    L9 JASON
    L9 JASON

--- assignments/lesson-9/scripts/currentweather.js
+++ assignments/lesson-9/scripts/currentweather.js
@@ -1,41 +1,13 @@
-var main = document.querySelector("main");
-var section = document.createElement("section");
-var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-var request = new XMLHttpRequest();
-request.open('GET', 'http://api.wunderground.com/api/67d090e8fbb75d1e/conditions/q/MN/Franklin.json', true);
-request.responseType = 'json';
-request.send();
-request.onload = function () {
-var weather = request.response;
-weatherInfo(weather);
-	 document.getElementById('place').innerHTML = weather.current_observation.display_location.full;
-}
+var weatherObject = new XMLHttpRequest();
 
-function weatherInfo(jsonObj) {
-	var townsArray = jsonObj["towns"];
-	for (var i = 0; i < townsArray.length; i++) {
-		if (townsArray[i].name != "Placerton") {
-			var mydiv = document.createElement("div");
-			var myh1 = document.createElement("h1");
-			var myp1 = document.createElement("p");
-			var myp2 = document.createElement("p");
-			var myp3 = document.createElement("p");
-			var myp4 = document.createElement("p");
+weatherObject.open('GET', 'http://api.wunderground.com/api/67d090e8fbb75d1e/conditions/q/MN/Franklin.json', true);
 
-			myh1.textContent = townsArray[i].name + " " + "City";
-			myp1.textContent = "Motto: " + " " + townsArray[i].motto;
-			myp2.textContent = "Year Founded:" + " " + townsArray[i].yearFounded;
-			myp3.textContent = "Current Population:" + " " + townsArray[i].currentPopulation;
-			myp4.textContent = "Average Rainfall:" + " " + townsArray[i].averageRainfall + " " + "inches";
+weatherObject.send();
 
-			mydiv.appendChild(myh1);
-			mydiv.appendChild(myp1);
-			mydiv.appendChild(myp2);
-			mydiv.appendChild(myp3);
-			mydiv.appendChild(myp4);
-
-			section.appendChild(mydiv);
-		}
-	}
-	main.appendChild(section);
+weatherObject.onload = function () {
+	var weatherInfo = JSON.parse(weatherObject.responseText);
+	console.log(weatherInfo);
+	document.getElementById('place').innerHTML = weatherInfo.current_observation.display_location.full;
+	document.getElementById('currentTemperature').innerHTML = weatherInfo.current_observation.temp_f;
+	document.getElementById('w_icon').src = weatherInfo.current_observation.icon_url;
 }
