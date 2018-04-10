51f6fee56cf89972fda0eda417cb2a71d698e139
Sandra S. Marler <SandraSMarler@MacBook-Pro-3.frontierlocal.net>
2018-04-10 18:15:27 -0400

    website
    website

--- assignments/themountainspoke.html/scripts/main.js
+++ assignments/themountainspoke.html/scripts/main.js
@@ -1,9 +1,10 @@
-$(document).ready(function () {
-	$('.next').on('click', function () {
+$(document).ready(function(){
+	$('.next').on('click', function(){
+		/*console.log('clicked');*/
 		var currentImg = $('.active');
 		var nextImg = currentImg.next();
 
-		if (nextImg.length) {
+		if (nextImg.length){
 			currentImg.removeClass('active').css('z-index', -10);
 			nextImg.addClass('active').css('z-index', 10);
 		}
@@ -13,14 +14,10 @@ $(document).ready(function () {
 		var currentImg = $('.active');
 		var prevImg = currentImg.prev();
 
-		if (prevImg.length) {
+		if (prevImg.length){
 			currentImg.removeClass('active').css('z-index', -10);
 			prevImg.addClass('active').css('z-index', 10);
 		}
 	});
-	$(document).ready(function () {
-		$('.prev').on('click', function () {
-			var prevImg = $('img.active').prev('.slider-inner img');
-		}
-					  });
-	});
+});
+
